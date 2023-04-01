9c6cbf2b1017af1a794ee0547635b9078b3edeb2
saki100 <sara.spanovic10@gmail.com>
2023-04-01 12:04:23 +0200

    Implementirana logicka kontrola u klasi Biblioteka.

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -10,22 +10,29 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null || knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne sme biti null i ne sme biti duplikat.");
          knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga==null || !knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne sme biti null i mora postojati u biblioteci.");
           knjige.remove(knjiga);
 	}
 
 	@Override
 	public LinkedList<Knjiga> vratiSveKnjige() {
+		//ova metoda ne treba da ima nikakvu log kontrolu, to je samo obican getter
 		return knjige;
 	}
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String Izdavac) {
-       LinkedList<Knjiga> rezultat=new LinkedList<>();
+      if(autor==null && isbn==null && naslov==null && izdavac==null)
+    	  throw new RuntimeException("Morate uneti barem 1 kriterijum pretrage");
+		LinkedList<Knjiga> rezultat=new LinkedList<>();
 		for(Knjiga knjiga:knjige)
 			if(knjiga.getNaslov().contains(naslov))
 				rezultat.add(knjiga);
